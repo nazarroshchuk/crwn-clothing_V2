d7dccaa6a0a7bcbc0208dd4ca8297c2f0e7490df
Nazarii <nazarrishchuk@gmail.com>
2022-09-22 16:44:42 +0300

    temp mac

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { UserProvider } from "./contexts/user.context";
 
+
 const rootElement = document.getElementById('root');
 
 render(
