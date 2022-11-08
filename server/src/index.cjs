fd465785249236c6885253ee2f5a3ce03bfbc864
n.roshchuk <nroshchuk@visualfabriq.com>
2022-11-08 17:40:58 +0200

    Shop comp & shop context

--- server/src/index.cjs
+++
@@ -1,6 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable @typescript-eslint/no-var-requires */
-require('@babel/register')({ extensions: ['.js', '.ts'] });
-const { startUp } = require('./server.ts');
-
-startUp();
