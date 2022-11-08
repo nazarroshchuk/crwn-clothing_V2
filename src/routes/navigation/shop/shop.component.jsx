fa83024478717be22d2a1da96498196b8c28da98
n.roshchuk <nroshchuk@visualfabriq.com>
2022-11-08 18:05:51 +0200

    Shop comp & shop context

--- src/routes/navigation/shop/shop.component.jsx
+++ src/routes/navigation/shop/shop.component.jsx
@@ -1,17 +1,16 @@
 import { useContext } from 'react';
 import { ProductContext } from '../../../contexts/products.context';
-
+import ProductCard from "../../../components/product-card/product-card.component";
+import './shop.styles.scss'
 const Shop = () => {
 
     const context = useContext(ProductContext);
 
     const products = context.products
     return (
-        <div>
-            {products.map(({id, name }) => (
-                <div key={id}>
-                    <h1>{name}</h1>
-                </div>
+        <div className={'products-container'}>
+            {products.map(product => (
+                <ProductCard key={product.id} product={product} />
             ))}
         </div>
     )
