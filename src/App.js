d1de739d8fcb19fc80cf549f940a3b72183abdd6
n.roshchuk <nroshchuk@visualfabriq.com>
2022-11-08 17:32:58 +0200

    Shop comp & shop context

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Authentication from "./routes/authenticarion/authentication.component";
-
-const Shop = () => {
-  return <h1>I am the shop page</h1>;
-};
+import Shop from './routes/navigation/shop/shop.component'
 
 const App = () => {
   return (
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
-import React from 'react';
-import { render } from 'react-dom';
-import './index.scss';
-import App from './App';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import { render } from "react-dom";
+import "./index.scss";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/user.context";
+import { ProductProvider } from "./contexts/products.context";
 
-
-const rootElement = document.getElementById('root');
+const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductProvider>
+          <App />
+        </ProductProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
