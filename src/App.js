9268e044cc0fca982e7a17769005dadfa4ebdd9f
n.roshchuk <nroshchuk@visualfabriq.com>
2022-12-28 16:35:47 +0200

    lazy loading

--- src/App.js
+++ src/App.js
@@ -1,31 +1,36 @@
-import { Routes, Route } from 'react-router-dom';
-import Home from './routes/home/home.component';
-import Navigation from './routes/navigation/navigation.component';
-import Authentication from "./routes/authenticarion/authentication.component";
-import Shop from './routes/navigation/shop/shop.component'
-import Checkout from "./routes/checkout/checkout.component";
-import { useEffect } from "react";
+import { lazy, useEffect, Suspense } from "react";
+import { Routes, Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { userActions } from "./store/user/user.actions";
+import { Spinner } from "./components/spinner/spinner.component";
 
-
+const Home = lazy(() => import("./routes/home/home.component"));
+const Navigation = lazy(() =>
+  import("./routes/navigation/navigation.component")
+);
+const Checkout = lazy(() => import("./routes/checkout/checkout.component"));
+const Shop = lazy(() => import("./routes/navigation/shop/shop.component"));
+const Authentication = lazy(() =>
+  import("./routes/authentication/authentication.component")
+);
 const App = () => {
-
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(userActions.checkUserSession());
-  }, [dispatch])
+  }, [dispatch]);
 
   return (
-    <Routes>
-      <Route path='/' element={<Navigation />}>
-        <Route index element={<Home />} />
-        <Route path='shop/*' element={<Shop />} />
-        <Route path='auth' element={<Authentication />} />
-        <Route path="checkout" element={<Checkout/>} />
-      </Route>
-    </Routes>
+    <Suspense fallback={<Spinner />}>
+      <Routes>
+        <Route path="/" element={<Navigation />}>
+          <Route index element={<Home />} />
+          <Route path="shop/*" element={<Shop />} />
+          <Route path="auth" element={<Authentication />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </Suspense>
   );
 };
 
